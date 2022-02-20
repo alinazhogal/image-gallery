70973a8b0b6e53e68a4e0de25f550edf98e4ceaf
Alina <alina.zhogal.00@gmail.com>
2022-02-20 13:00:11 +0300

    feat: search on input

--- image-gallery/index.js
+++ image-gallery/index.js
@@ -9,11 +9,16 @@ input.addEventListener('keydown', (event)=> {
 async function getImages() {
   removeImages();
 
-  const url = `https://api.unsplash.com/search/photos?query=${input.value}&per_page=20&orientation=landscape&client_id=2HzxQY_bB-qJY9T-cO40am_Hna-k5vorpjX-fr1-t0s`;
+  const url = `https://api.unsplash.com/search/photos?query=${input.value}&per_page=21&orientation=landscape&client_id=2HzxQY_bB-qJY9T-cO40am_Hna-k5vorpjX-fr1-t0s`;
 
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data);
+  const imageNodes = [];
+  for (let i = 0; i < data.results.length; i++) {
+    imageNodes[i] = document.createElement('img');
+    imageNodes[i].src = data.results[i].urls.raw;
+    section.append(imageNodes[i])
+  }
 }
 
 function removeImages() {
